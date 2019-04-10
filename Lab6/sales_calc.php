96887c6648c789ed144d196ddb32003c95c151de
chrischism8063 <chrischism8063@gmail.com>
2019-04-10 11:31:54 -0400

    pushing changes still need to work on the store

--- Lab6/sales_calc.php
+++ Lab6/sales_calc.php
@@ -10,22 +10,41 @@
     <h1>Sales Tax Calculator</h1>
     <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
         <?php
-            var_dump($_POST);
-            echo "NOTHING HERE";
-            if(isset($_POST['submit'])){
-                echo "NOTHING REALLY HERE";
-                echo "This is working";
-                $rate = $_POST['rate'];
-                echo "<p>Rate is:" .$rate ."</p>";
-                $amount = $_POST['amount'];
-                echo "Amount is: " .$amount ."</p>";
-            }else{
-                echo "Amount: <input type='text' name='amount'>";
-                echo "Tax rate: <input type='text' name='rate'>";
-                echo "<input type='button' value='Submit' name='submit'>";        
-            }
             
-            ?>
+            function displayForm($error1, $error2){
+                echo "Amount: <input type='text' name='amount' required><br />" .$error2 ."<br />";
+                echo "Tax rate: <input type='text' name='rate' required><br />" .$error1 ."<br />";
+                echo "<input type='submit' name='submit' value='Submit'>";
+            }
+
+            if(isset($_POST['submit'])){
+                if(empty($_POST['rate']) || !(is_numeric($_POST['rate']))){
+                    $error1 = "Please enter a numeric value for the rate!<br />";
+                    displayForm($error1, $error2);
+                }
+                if(empty($_POST['amount']) || !(is_numeric($_POST['amount']))){ 
+                    $error2 = "Please enter a numberic value for the amount!<br />";
+                    displayForm($error1, $error2);
+                }
+                if(empty($error1) && empty($error2)){
+                    $rate = $_POST['rate'];
+                    $amount = $_POST['amount'];
+                    $total = (($rate / 100) * $amount) + $amount;
+
+                    echo "Entered Amount: " .$amount ."<br />";
+                    echo "Enerted Tax Rate: " .$rate ."% <br />";
+                    echo "Calculated Tax Amount:$" .number_format($total, 2) ."<br />";
+                    echo "<input type='submit' name='return' value='Return'>";
+                }
+            }else if(isset($POST['return'])){
+                $rate = 0;
+                $amount = 0;
+                $total = 0;
+                displayForm($error1, $error2);
+            }else{
+                displayForm($error1, $error2);
+            }
+        ?>
     </form>
 </body>
 </html>
