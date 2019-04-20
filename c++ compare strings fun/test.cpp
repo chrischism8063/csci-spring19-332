de13ecfb1b7a18d05051a43961befba24fb4291e
chrischism8063 <XXXxx22??>
2019-04-20 18:25:00 -0400

    pushing

---
+++ c++ compare strings fun/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string var_one = "something";
+    string var_two = "SOMETHING";
+    char test[90] = "somting";
+
+    cout << var_one.compare("somting") << endl;
+
+
+    return 0;
+}
